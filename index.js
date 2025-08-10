db68f8682d95eb5c476d66f3ba6ac0e60a176f92
Siddesh-2004 <siddeshpoojary2004@gmail.com>
2025-08-11 01:46:52 +0530

    project completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import * as fs from 'node:fs';
+import * as fs from "node:fs";
+
+const destinationPath = process.argv[2];
+if (!destinationPath) {
+  console.log("path is required as a command line argument");
+  process.exit();
+}
+console.log("Installation started")
+fs.cpSync("backend", destinationPath, { recursive: true }, (err) => {
+  if (err) console.log(err);
+});
+console.log("Installation completed");
